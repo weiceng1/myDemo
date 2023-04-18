d94608c8a3be7420373ca58f49ec7731f5e4c2b7
shuangshuang.wu <shuangshuang.wu@accenture.com>
2023-04-18 11:51:56 +0800

    add dependencies

---
+++ src/main/java/com/demo/MainApplication.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("my demo");
+    }
+}
